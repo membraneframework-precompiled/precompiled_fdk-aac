51e2e48ede765d433d4e78d28cfe3598e3eccd22
Noarkhh <jakub.pryc@gmail.com>
2023-09-25 13:49:22 +0200

    Init

---
+++ build_portaudio_macos_intel.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/project/workspace/$ARTIFACT_NAME/include
+mkdir -p ~/project/workspace/$ARTIFACT_NAME/lib
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install fdk-aac
+cp -r /usr/local/include/* ~/project/workspace/$ARTIFACT_NAME/include
+cp -r /usr/local/lib/*.dylib ~/project/workspace/$ARTIFACT_NAME/lib
+cd ~/project/workspace/$ARTIFACT_NAME/lib
+for f in *.dylib;
+do
+    install_name_tool -id "@rpath/$f" $f
+done
